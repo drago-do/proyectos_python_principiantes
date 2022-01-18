81ca219ded66f282553641c6d70c27ec8479d9b8
Drago <dragodominguezfb@gmail.com>
2022-01-17 18:36:32 -0600

    1/4 de proyecto madlib

---
+++ historiasLocasa-madlib/madlib.py
@@ -0,0 +1,13 @@
+# En este primer proyecto aprenderemos la concatenacion de caracteres en python usando diferentes metodos
+# Para esto haremos un proyecto sencillo llamado "historias locas" (del ingles "madlib") en el cual
+# pediremos al usuario que escriba verbos o sustantivos segun corresponda para armar una historia graciosa.
+
+# Existen diferentes maneras de concatenar en python, estas son 3 de las mas comunes
+
+    #Declaramos una variable
+    canalYoutube = ""  #Alguna cadena de texto
+
+print("Subscribete al canal " + canalYoutube)
+print("Subscribete al canal {}".format(canalYoutube))
+print(f"Subscribete al canal {}")
+
